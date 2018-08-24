7b3327c48b3c7e699b81773b86f9e9e5bb99b328
Kacper Latuszewski <kacpx123@gmail.com>
2018-08-24 23:45:14 +0200

    Add basic hero image view

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,75 +1,61 @@
 <template>
   <div class="wrapper">
-    <div class="search">
-      <label for="search">Search</label>
-      <input
-        id="search"
-        name="search" 
-        v-model="searchValue"
-        @input="handleInput"
-      />
-      <ul>
-        <li v-for="item in results" :key="item.data[0].nasa_id"> 
-          <p>{{ item.data[0].description }}</p>
-        </li>
-      </ul>
-    </div>
+    <Claim />
+    <SearchInput />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import debounce from 'lodash.debounce'
+import axios from "axios";
+import debounce from "lodash.debounce";
+import Claim from "@/components/Claim";
+import SearchInput from "@/components/SearchInput";
 
-const API = 'https://images-api.nasa.gov/search'
+const API = "https://images-api.nasa.gov/search";
 
-  export default {
-    name: 'Search',
-    data() {
-      return {
-        searchValue: '',
-        results: [],
-      }
-    },
-    methods: {
-      // eslint-disable-next-line 
-      handleInput: debounce(function () {
-        axios.get(`${API}?q=${this.searchValue}&media_type=image`) 
-          .then((response) => {
-            this.results = response.data.collection.items;
-          })
-          .catch((error) => {
-            });
-            console.log(error);
-    }, 500),
+export default {
+  name: "Search",
+  components: {
+    Claim,
+    SearchInput
   },
+  data() {
+    return {
+      searchValue: "",
+      results: []
+    };
+  },
+  methods: {
+    // eslint-disable-next-line
+    handleInput: debounce(function() {
+      axios
+        .get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then(response => {
+          this.results = response.data.collection.items;
+        })
+        .catch(error => {});
+      console.log(error);
+    }, 500)
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 0;
-    padding: 30px;
-    width: 100%;
-  }
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  background-image: url('../assets/heroimage.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 80% 0%;
+}
 
-  .search {
-    display: flex;
-    flex-direction: column;
-    width: 250px;
 
-    label {
-      font-family: Montserrat, sans-serif;
-    }
-
-    input {
-      height: 30px;
-      border: 0;
-      border-bottom: 1px solid black;
-    }
-  }
 </style>
 
